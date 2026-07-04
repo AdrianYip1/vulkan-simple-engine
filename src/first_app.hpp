36b69e18c8fc28336ce240458c4b01a0dbe214a0
Adrian Yip <yipadrian1119@gmail.com>
2026-07-03 19:26:36 -0600

    feat: triangle rendered -> pipeline, comand buffers, swap chain

--- src/first_app.hpp
+++ src/first_app.hpp
@@ -3,6 +3,10 @@
 #include "tut_window.hpp"
 #include "tut_pipeline.hpp"
 #include "tut_device.hpp"
+#include "tut_swap_chain.hpp"
+
+#include <memory>
+#include <vector>
 
 namespace tut {
 	class FirstApp {
@@ -11,11 +15,25 @@ namespace tut {
 		static constexpr int WIDTH = 800;
 		static constexpr int HEIGHT = 600;
 
+		FirstApp();
+		~FirstApp();
+
+		FirstApp(const FirstApp&) = delete;
+		FirstApp& operator=(const FirstApp&) = delete;
 		void run();
 
 	private:
+		void createPipelineLayout();
+		void createPipeline();
+		void createCommandBuffers();
+		void drawFrame();
+
+
 		TutWindow tutWindow{ WIDTH, HEIGHT, "Vulkan Window" };
 		TutDevice tutDevice{ tutWindow };
-		TutPipeline tutPipeline{ tutDevice, "shaders/simple_shader.vert.spv", "shaders/simple_shader.frag.spv", TutPipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT) };
+		TutSwapChain tutSwapChain{ tutDevice, tutWindow.getExtent()};
+		std::unique_ptr<TutPipeline> tutPipeline;
+		VkPipelineLayout pipelineLayout;
+		std::vector<VkCommandBuffer> commandBuffers;
 	};
 }
--- src/tut_pipeline.hpp
+++ src/tut_pipeline.hpp
@@ -6,7 +6,20 @@
 
 namespace tut {
 
-	struct PipelineConfigInfo {};
+	struct PipelineConfigInfo {
+		VkViewport viewport;
+		VkRect2D scissor;
+	
+		VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+		VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+		VkPipelineMultisampleStateCreateInfo mutisampleInfo;
+		VkPipelineColorBlendAttachmentState colorBlendAttachment;
+		VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+		VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+		VkPipelineLayout pipelineLayout = nullptr;
+		VkRenderPass renderPass = nullptr;
+		uint32_t subpass = 0;
+	};
 
 	class TutPipeline {
 	public:
@@ -14,10 +27,12 @@ namespace tut {
 			const std::string& vertFilepath,
 			const std::string& fragFilepath,
 			const PipelineConfigInfo& configInfo);
-		~TutPipeline() {};
+		~TutPipeline();
 
 		TutPipeline(const TutPipeline&) = delete;
 		void operator=(const TutPipeline&) = delete;
+
+		void bind(VkCommandBuffer commandBuffer);
 
 		static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
 
