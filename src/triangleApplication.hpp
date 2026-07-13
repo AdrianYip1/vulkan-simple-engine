0d72455374a40d41ca473e843c119a91d2e45999
Adrian Yip <yipadrian1119@gmail.com>
2026-07-13 15:05:59 -0600

    feat: finished debug message log, window, surface, extensions, and physical/logicand physical/logical devices

--- src/triangleApplication.hpp
+++ src/triangleApplication.hpp
@@ -7,6 +7,24 @@
 #include <stdexcept>
 #include <cstdlib>
 #include <vector>
+#include <cstring>
+#include <optional>
+#include <set>
+
+struct QueueFamilyIndices {
+	std::optional<uint32_t> graphicsFamily;
+	std::optional<uint32_t> presentFamily;
+
+	bool isComplete() {
+		return graphicsFamily.has_value() && presentFamily.has_value();
+	}
+};
+
+struct SwapChainSupportDetails {
+	VkSurfaceCapabilitiesKHR capabilities;
+	std::vector<VkSurfaceFormatKHR> formats;
+	std::vector<VkPresentModeKHR> presentModes;
+};
 
 class TriangleApplication {
 public:
@@ -15,15 +33,49 @@ public:
 private:
 	GLFWwindow* window;
 	VkInstance instance;
+	VkDebugUtilsMessengerEXT debugMessenger;
+	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+	VkDevice device;
+	VkQueue graphicsQueue;
+	VkQueue presentQueue;
+	VkSurfaceKHR surface;
 
 	void initWindow();
 
 	void initVulkan();
 
+	void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+
+	void setupDebugMessenger();
+
 	void createInstance();
 
 	bool checkValidationLayerSupport();
+
+	std::vector<const char*> getRequiredExtensions();
+
+	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+		VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+		VkDebugUtilsMessageTypeFlagsEXT messageType,
+		const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+		void* pUserData);
+
+	void pickPhysicalDevice();
+
+	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+
+	bool isDeviceSuitable(VkPhysicalDevice device);
+
+	bool checkDeviceExtensionSupport(VkPhysicalDevice device);
+
+	void createLogicalDevice();
+
+	void createSurface();
+
+	SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
+
 	void mainLoop();
+
 	void cleanup();
 
 
