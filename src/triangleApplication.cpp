0f7d2de4b5f8d36f4c7984a54064b4b77c3d939d
Adrian Yip <yipadrian1119@gmail.com>
2026-07-19 14:39:59 -0600

    fix: rasterizer needed to have the cull for back faces set to ccw as the y for coordinates was inverted

--- src/triangleApplication.cpp
+++ src/triangleApplication.cpp
@@ -595,7 +595,7 @@ void TriangleApplication::createGraphicsPipeline() {
 	rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
 	rasterizer.lineWidth = 1.0f;
 	rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
-	rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
+	rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
 	rasterizer.depthBiasEnable = VK_FALSE;
 	rasterizer.depthBiasConstantFactor = 0.0f;
 	rasterizer.depthBiasClamp = 0.0f;
@@ -1087,8 +1087,7 @@ void TriangleApplication::updateUniformBuffer(uint32_t currentImage) {
 	float aspect = swapChainExtent.width / (float)swapChainExtent.height;
 
 	UniformBufferObject ubo{ enginemath::Mat4::identity(), enginemath::Mat4::identity(), enginemath::Mat4::identity() };
-	enginemath::Mat4 _model = enginemath::Mat4::identity() *
-							  enginemath::Mat4::rotateZ(enginemath::toRad(sin(time)));
+	enginemath::Mat4 _model = enginemath::Mat4::rotateZ(90 * sin(enginemath::toRad(time)));
 	
 	enginemath::Mat4 _view = enginemath::Mat4::lookAtM(enginemath::Vec3(2.0f, 2.0f, 2.0f), 
 												       enginemath::Vec3(0.0f, 0.0f, 0.0f), 
